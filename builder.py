fe18963f659ac97882c8e81bae84b68f1e3cdd38
Touhid Arastu <touhid.arastu@gmail.com>
2018-12-25 16:49:12 +0000

    add pipfile

--- builder.py
+++ builder.py
@@ -1,18 +1,18 @@
-import csv
-import json
+from csv import reader
+from json import dump
 from netaddr import iprange_to_cidrs
 
 ranges = set()
 
 with open('iran_raw_data.csv', newline='') as csvfile:
-    r = csv.reader(csvfile)
+    r = reader(csvfile)
     for row in r:
         cidrs = iprange_to_cidrs(row[0], row[1])
         for ip_range in cidrs:
             ranges.add(str(ip_range))
 
 with open('iran_ip_range.json', 'w') as outfile:
-    json.dump(list(ranges), outfile, sort_keys=True, indent=4)
+    dump(list(ranges), outfile, sort_keys=True, indent=4)
 
 with open('iran_ip_range.txt', 'w') as fp:
     for r in ranges:
